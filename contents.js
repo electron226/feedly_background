ea90c2c8e1f98ff2ec034fb1543b8d4a78216de4
electron226 <electron226@gmail.com>
2013-03-15 19:41:42 +0900

    プレビュー画面を表示している際にも開けるよう対応。

--- contents.js
+++ contents.js
@@ -6,23 +6,43 @@ if (!opentab) {
                 return;
             }
 
-            var entries = document.evaluate(
-                '//div[contains(@class, "selectedEntry")]',
+            // Find full preview
+            var previews = document.evaluate(
+                '//iframe[contains(@id, "quicklookContent")]',
                 document, null, 7, null);
-            if (entries.snapshotLength != 1) {
-                return;
-            }
-            var entry = entries.snapshotItem(0);
+            if (previews.snapshotLength == 1) { // Found full preview
+                var url = preview.snapShotItem(0).src;
+            } else { // can't find  fullpreview
+                // Find select entry
+                var entries = document.evaluate(
+                    '//div[contains(@class, "selectedEntry")]',
+                    document, null, 7, null);
+                if (entries.snapshotLength != 1) {
+                    return;
+                }
+                var entry = entries.snapshotItem(0);
+                var entryId = entry.id;
 
-            var urls = document.evaluate(
-                '//a[contains(@id, "' + entry.id + '_title")]',
-                entry, null, 7, null);
-            if (urls.snapshotLength != 1) {
-                return;
+                // Do select entry is mini preview?
+                var i = entryId.lastIndexOf("_abstract");
+                if (i != -1) {
+                    // delete '_abstract'
+                    entryId = entryId.substr(0, i);
+                }
+
+                // Find url of select entry.
+                var urls = document.evaluate(
+                    '//a[contains(@id, "' + entryId + '_title")]',
+                    entry, null, 7, null);
+                if (urls.snapshotLength != 1) {
+                    return;
+                }
+
+                var url = urls.snapshotItem(0).href;
             }
 
-            var url = urls.snapshotItem(0).href;
             if (url) {
+                // This extension will open url.
                 chrome.extension.sendMessage({ "url": url });
             }
         });
